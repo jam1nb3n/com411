75142e4c451120892fb35cd19a43e3dbf1c77ef1
repl.it user <replituser@example.com>
2020-09-29 16:07:13 +0000

    additional console output

--- basics/output/simple_message.py
+++ basics/output/simple_message.py
@@ -1,2 +1,5 @@
 print("System Failure Imminent!")
 print("System reboot has been initiated...")
+print("\n\n...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine\n\nSytem online.")

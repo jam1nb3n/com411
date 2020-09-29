fb369ea11a4613fba732f89b0279c3a9e3d2bdb0
repl.it user <replituser@example.com>
2020-09-29 16:41:13 +0000

    BMI Calculator

---
+++ basics/input/data_types.py
@@ -0,0 +1,11 @@
+name = input("Name? ")
+age = int(input("Age in years? "))
+height = float(input("Height in metres? "))
+weight = float(input("Weight in Kg? "))
+
+print("Name: ", name)
+print("Age:", str(age))
+print("Height: ", str(round(height, 2)))
+print("Weight: ", str(round(weight, 2)))
+BMI = weight / (height ** 2)
+print("BMI: ", round(BMI, 2))

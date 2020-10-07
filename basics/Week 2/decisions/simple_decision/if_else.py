05e48cdc0632af960d4cdf6d54f1891ddbd2f6d2
jam1nb3n <unconfigured@null.spigotmc.org>
2020-10-07 11:59:49 +0100

    Week 2 - if_else.py

---
+++ basics/Week 2/decisions/simple_decision/if_else.py
@@ -0,0 +1,6 @@
+activity = input("Please enter the activity to be performed: ")
+if(activity == "calculate"):
+    print("Performing calculations...\n")
+else:
+    print("Performing activity...\n")
+print("Activity completed!")

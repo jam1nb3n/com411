7746f9f1c317d188bdeaab7a099a119520e21300
jam1nb3n <unconfigured@null.spigotmc.org>
2020-10-07 11:55:41 +0100

    Week 2 - if.py

---
+++ basics/Week 2/decisions/simple_decision/if.py
@@ -0,0 +1,4 @@
+bookType = input("What type of book is this?: ") # Asks type of book
+if(bookType == "adventure"):    
+    print("I like adventure books") # Print I like adventure books if bookType is adventure
+print("\nFinished reading book.") # Print finished reading book

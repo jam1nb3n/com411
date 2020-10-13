b2859797332f54d86081c66362304437b00597b6
jam1nb3n <unconfigured@null.spigotmc.org>
2020-10-13 10:17:13 +0100

    Added code to demonstrate the use of a logical and operator.

---
+++ basics/Week 2/decisions/and_operator.py
@@ -0,0 +1,7 @@
+hearInput = input("What did I hear?\n")
+seeInput = input("What did I see?\n")
+
+if ((hearInput == "grr") and (seeInput == "two red eyes")):
+    print("There is a scary creature. I should get out of here!")
+else:
+    print("I am a little scared but I will continue.")

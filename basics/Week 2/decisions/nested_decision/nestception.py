04b490ea0d68a1f8b8e52ec458af48591dc6eda5
jam1nb3n <unconfigured@null.spigotmc.org>
2020-10-13 09:47:44 +0100

    Added code to demonstrate the use of multiple nested statements.

---
+++ basics/Week 2/decisions/nested_decision/nestception.py
@@ -0,0 +1,23 @@
+lookLocation = input("Where should I look?\n")
+
+if (lookLocation == "in the bedroom"):
+   lookLocationBed = input("Where in the bedroom should I look?\n")
+   if (lookLocationBed == "under the bed"):
+       print("Found some shoes but no battery")
+    else:
+        print("Found some mess but no battery.")
+elif (lookLocation == "in the bathroom"):
+    lookLocationBath = input("Where in the bathroom should I look?\n")
+    if (lookLocationBath == "in the bathtub"):
+        print("Found a rubber duck but no battery")
+    else:
+        print("Found a wet surface but no battery.")
+elif (lookLocation == "in the lab"):
+    lookLocationLab = input("Where in the lab should I look?")
+    if (lookLocationLab == "on the table"):
+        print("Yes! I found my battery!")
+    else:
+        print("Found some tools but no battery.")
+else:
+    print("I do not know where that is but I will keep looking.")
+

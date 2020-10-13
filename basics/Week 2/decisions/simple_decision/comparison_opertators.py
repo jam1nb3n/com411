970f9be58cb364b7eba0b9578d7fadab28d4cccf
jam1nb3n <unconfigured@null.spigotmc.org>
2020-10-13 09:07:02 +0100

    Added code to demonstrate the use of comparison operators.

---
+++ basics/Week 2/decisions/simple_decision/comparison_opertators.py
@@ -0,0 +1,7 @@
+numOne = int(input("Please enter the first number: "))
+numTwo = int(input("Please enter the second number: "))
+
+if (numOne > numTwo):
+    print(f"{numOne} is largest")
+else:
+    print(f"{numTwo} is largest")

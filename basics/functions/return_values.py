c91d0a4b52004ae81fbe2bcb9e8cb66aa9001309
Benjamin Newman <benewman2002@gmail.com>
2020-10-15 17:05:38 +0100

    Added code to demonstrate the use of return values.

---
+++ basics/functions/return_values.py
@@ -0,0 +1,16 @@
+def sum_weights(beep, bop):
+    return (beep+bop)
+
+def calc_avg_weight (beep, bop):
+    return ((sum_weights(beep, bop))/2)
+
+def run():
+    beep = int(input("Beep: "))
+    bop = int(input("Bop: "))
+    runMode = input("What would you like to calculate (sum or average)?\n")
+    if (runMode == "sum"):
+        print(sum_weights(beep, bop))
+    if (runMode == "average"):
+        print(calc_avg_weight(beep, bop))
+
+run()
